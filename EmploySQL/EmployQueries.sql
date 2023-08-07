ec7dfac8e74d502de75c6ae69f29d8762a793583
Sari Rojas <sari.stormdance@gmail.com>
2023-08-06 22:29:41 -0500

    creating tables

---
+++ EmploySQL/EmployQueries.sql
@@ -0,0 +1,28 @@
+-- CREATE TABLE Departments (
+-- 	Dept_no VARCHAR(10),
+-- 	Dept_name VARCHAR(30));
+	
+-- CREATE TABLE Dept_emp (
+-- 	Emp_no INT,
+-- 	Dept_no VARCHAR(10));
+	
+-- CREATE TABLE Dept_manager (
+-- 	Dept_no VARCHAR(10),
+-- 	Emp_no INT);
+	
+-- CREATE TABLE Employees (
+-- 	Emp_no INT,
+-- 	Emp_Title VARCHAR(30),
+-- 	Birthdate VARCHAR (20),
+-- 	First_name VARCHAR (30),
+-- 	Last_name VARCHAR (30),
+-- 	Sex VARCHAR (1),
+-- 	Hire_date VARCHAR(20));
+	
+-- CREATE TABLE Salaries (
+-- 	Emp_no INT
+-- 	Salary INT);
+	
+-- CREATE TABLE Titles (
+-- Title_id VARCHAR(10),
+-- Title VARCHAR(30));
