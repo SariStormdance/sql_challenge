ae45429d12db0467aebda3424b697451105a2cb8
Sari Rojas <sari.stormdance@gmail.com>
2023-08-16 21:58:36 -0500

    Shuffling

--- EmploySQL/EmployQueries.sql
+++ EmploySQL/EmployQueries.sql
@@ -1,35 +1,44 @@
--- Create Table Departments (
--- 	Dept_no VARCHAR(10) NOT NULL,
--- 	Dept_name VARCHAR(30),
--- 	Primary Key (Dept_no)
+-- CREATE TABLE Titles( 
+-- 	Title_id VARCHAR(10),
+-- 	Title VARCHAR(30),
+-- 	PRIMARY KEY (Title_id)
 -- );
-
--- Create Table dept_emp (
--- emp_no INTEGER NOT NULL,
--- dept_no VARCHAR(5)
--- );
-
--- CREATE TABLE Dept_manager (
--- 	Dept_no VARCHAR(5),
--- 	Emp_no INTEGER NOT NULL);
 
 -- CREATE TABLE Employees (
--- 	emp_no INTEGER NOT NULL,
+-- 	Emp_no INTEGER NOT NULL,
 -- 	Emp_Title VARCHAR(30),
 -- 	Birthdate VARCHAR (20),
 -- 	First_name VARCHAR (30),
 -- 	Last_name VARCHAR (30),
 -- 	Sex VARCHAR (1),
--- 	Hire_date VARCHAR(20)
+-- 	Hire_date VARCHAR(20),
+-- 	PRIMARY KEY (Emp_no),
+-- 	FOREIGN KEY (Emp_Title) REFERENCES Titles(title_id)
+-- );
+
+-- CREATE TABLE dept_emp (
+-- 	Emp_no INTEGER NOT NULL,
+-- 	Dept_no VARCHAR(5) NOT NULL,
+-- 	PRIMARY KEY (emp_no, dept_no)
+-- );
+
+-- CREATE TABLE Dept_manager (
+-- 	Dept_no VARCHAR(5) NOT NULL,
+-- 	Emp_no INTEGER NOT NULL,
+-- 	PRIMARY KEY (dept_no, emp_no)
+-- );
+
+-- Create Table Departments (
+-- 	Dept_no VARCHAR(5),
+-- 	Dept_name VARCHAR(30),
+-- 	Primary Key (Dept_no)
 -- );
 
 -- CREATE TABLE Salaries (
 -- 	Emp_no INT NOT NULL,
--- 	Salary INT);
-
--- CREATE TABLE Titles (
--- Title_id VARCHAR(10),
--- Title VARCHAR(30));
+-- 	Salary INT,
+-- 	PRIMARY KEY (Emp_no)
+-- );
 
 -- Select 
 -- Emp_no,
